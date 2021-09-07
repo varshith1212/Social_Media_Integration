64c09dbf41f1e7435d7c8f8db1c9dc8d4791f9ec
Varshith <doddasaivarshith1212@gmail.com>
2021-09-07 23:06:56 +0530

    Added the code to connect with the facebook account using facebook sdk and api.

--- app/src/main/java/com/example/socialmediaintegration/MainActivity.java
+++ app/src/main/java/com/example/socialmediaintegration/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.socialmediaintegration;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
@@ -16,6 +20,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.socialmediaintegration.databinding.ActivityMainBinding;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -24,10 +39,24 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ID_Email = "email_id";
+    public static final String ID_Name = "name_id";
 
     ActivityMainBinding binding;
     private GoogleSignInClient mGoogleSignInClient;
+
+    private TextView e;
+    private LoginButton login_button;
+    private CallbackManager c;
+    private ImageView i;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,7 +85,88 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult.launch(signInIntent);
             }
         });
+
+
+        // FaceBook implementation
+
+        //e = findViewById(R.id.e);
+        login_button = findViewById(R.id.login_button);
+        c = CallbackManager.Factory.create();
+
+        login_button.setPermissions(Arrays.asList("Email","UserBirthday"));
+
+        login_button.registerCallback(c, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                /*
+                //TODO: Use the Profile class to get information about the current user.
+                handleSignInResult(new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        LoginManager.getInstance().logOut();
+                        return null;
+                    }
+                });
+                */
+
+            }
+
+            @Override
+            public void onCancel() {
+                handleSignInResult(null);
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(MainActivity.class.getCanonicalName(), error.getMessage());
+                handleSignInResult(null);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        c.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker t = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null){
+                e.setText("");
+                Toast.makeText(MainActivity.this,"logout",Toast.LENGTH_SHORT).show();
+            }
+            else{
+                loadUserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                if (object != null){
+                    try{
+                        String email = object.getString("email");
+                        String id = object.getString("id");
+                        e.setText(email);
+                    }
+                    catch (JSONException jsonException) {
+                        jsonException.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
 
     ActivityResultLauncher<Intent> startActivityForResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -80,9 +190,17 @@ public class MainActivity extends AppCompatActivity {
                 String personFamilyName = acct.getFamilyName();
                 String personEmail = acct.getEmail();
                 String personId = acct.getId();
-                Uri personPhoto = acct.getPhotoUrl();
+                //Uri personPhoto = acct.getPhotoUrl();
 
-                Toast.makeText(this,"Email: "+personEmail,Toast.LENGTH_SHORT).show();
+                Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show();
+
+                Intent googleProfile = new Intent(MainActivity.this,sighIn.class);
+                String message = personEmail;
+                googleProfile.putExtra(ID_Email, personEmail);
+                googleProfile.putExtra(ID_Name,personName);
+                googleProfile.putExtra("ID_Photo", acct.getPhotoUrl().toString());
+
+                startActivity(googleProfile);
             }
 
         } catch (ApiException e) {
