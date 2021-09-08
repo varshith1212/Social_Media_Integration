a3569894cac4d352ed3dc6a4bff37825c1501b71
Varshith <doddasaivarshith1212@gmail.com>
2021-09-08 23:24:45 +0530

    Removed unused import statements

--- app/src/main/java/com/example/socialmediaintegration/sighIn.java
+++ app/src/main/java/com/example/socialmediaintegration/sighIn.java
@@ -3,7 +3,6 @@ package com.example.socialmediaintegration;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import com.bumptech.glide.Glide;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
@@ -24,8 +23,6 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.squareup.picasso.Picasso;
-
-import java.io.File;
 
 public class sighIn extends AppCompatActivity {
 
